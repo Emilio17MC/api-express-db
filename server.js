0d055e83869f5dfb12e0c12431aa41a7d0db2b85
Emilio <emilio17mc@gmail.com>
2022-05-08 13:40:35 -0500

    Endpoint GET /mc/:id

--- server.js
+++ server.js
@@ -123,6 +123,12 @@ app.get('/mc', async (req, res) => {
   res.json(allCommanders);
 });
 
+app.get('/mc/:id', async (req, res) => {
+  const id = req.params.id;
+  const missionCommander = await prisma.missionCommander.findUnique({where: {id:parseInt(id)}});
+  res.json(missionCommander);
+});
+
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
